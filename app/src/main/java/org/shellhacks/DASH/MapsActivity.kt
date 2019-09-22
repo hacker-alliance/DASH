0469b62bb375d016c250dbc166095967e4c10cb3
Terrell Ibanez <9453223+ibanezt@users.noreply.github.com>
2019-09-22 00:14:23 -0400

    Update Minimum Zoom Preference to Force Show Buildings

--- app/src/main/java/org/shellhacks/DASH/MapsActivity.kt
+++ app/src/main/java/org/shellhacks/DASH/MapsActivity.kt
@@ -35,9 +35,11 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        // Add a marker for FIU and Move Camera
+        val FIU = LatLng(25.75562, -80.3726)
+        mMap.addMarker(MarkerOptions().position(FIU).title("FIU Marker"))
+        //20 Minimum Zoom to Show Buildings
+        mMap.setMinZoomPreference(20.0f)
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(FIU))
     }
 }
